42bef82022bf92b14f39176e6079481e6601fc54
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-04 17:36:39 +0700

    :sparkles: :fuck deno i'm going back to fastify

--- api/controller.ts
+++
@@ -1,6 +0,0 @@
-const addTransaction = ({ request, response }: { request: any; response: any }) => {
-    response.body = { message: 'OK'} 
-    response.status = 200
-}
-
-export { addTransaction }
