6723c4ca0f94bf92a224644f869dcda9ebe9e069
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-04 14:43:15 +0700

    :sparkles: :wrench fix await

--- api/app.ts
+++ api/app.ts
@@ -1,7 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./routes.ts";
-
-const HOST = "127.0.0.1";
 const PORT = 7700;
 
 const app = new Application()
@@ -9,5 +7,4 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 
 console.log(`Listening on port ${PORT}...`)
-await app.listen(`${HOST}:${PORT}`)
 
