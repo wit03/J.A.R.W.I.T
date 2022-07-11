8edcce4f8f9894ef1f485e617a522a8874e2ac7f
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-11 23:53:02 +0700

    :fire:

--- src/router.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import indexController from "./controller/indexController";
-import postController from "./controller/postController"
-
-export default async function router(fastify: FastifyInstance) {
-  fastify.register(indexController, { prefix: "/" });
-  fastify.register(postController, {prefix: "/post"})
-}
