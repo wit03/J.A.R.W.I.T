7dc1e41bedd4504a015bf04e3a1f879a2dd970a8
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-12 11:28:35 +0700

    :sparkles: :feat debugginh

--- functions/controller/linehookController.ts
+++ functions/controller/linehookController.ts
@@ -1,11 +1,17 @@
 import { channel } from "diagnostic_channel";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import axios from "axios";
+import { request } from "http";
 
 const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
 
 export default async function linehookController(fastify: FastifyInstance) {
   fastify.post("/", async (_request: any, reply: FastifyReply) => {
+    for(const event of _request.body.events) {
+        if(event.type === 'message') {
+            console.log(event)
+        }
+    }  
     if (_request.body.type === "message") {
       axios.post("https://api.line.me/v2/bot/message/reply", {
         replyToken: _request.body.replyToken,
