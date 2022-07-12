0c76c4552cc89660fbf728bcf52f5740ebd9278c
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-12 14:09:38 +0700

    :wrench: :fix typo

--- functions/router.ts
+++ functions/router.ts
@@ -3,7 +3,7 @@ import indexController from "./controller/indexController"
 import linehookController from "./controller/linehookController"
 import { Client } from "@line/bot-sdk"
 
-const channelToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
+const channelToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 const client = new Client({
     channelAccessToken: channelToken!
 })
