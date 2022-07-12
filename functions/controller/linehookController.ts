69785019572b925dd96bd5ac88f343ffcf2f4c89
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-12 16:11:12 +0700

    :sparkles: :security restricted user to me

--- functions/controller/linehookController.ts
+++ functions/controller/linehookController.ts
@@ -2,14 +2,26 @@ import { channel } from "diagnostic_channel";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { client } from "../router";
 
-const channelToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
+const channelToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 
 export default async function linehookController(fastify: FastifyInstance) {
-   
-    client.replyMessage(channelToken!, {
-        type: "text",
-        text: "Test"
-    })
-    return "ok";
+  fastify.post("/", async (_request: any, reply: FastifyReply) => {
+    Promise
+      .all(_request.body.events.map(eventHandler))
+      .then((result) => reply.send(result));
+  });
 
+  const eventHandler = (event) => {
+    if (event.type !== "message" || event.message.type !== "text") {
+      return Promise.resolve(null);
+    } else if (event.source.userId !== process.env.LINE_USERID) {
+      console.warn("Someone try to use your bot");
+    } else {
+      console.log(event);
+      return client.replyMessage(event.replyToken, {
+        type: "text",
+        text: "Hi",
+      });
+    }
+  };
 }
