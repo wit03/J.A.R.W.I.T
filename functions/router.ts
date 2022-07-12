820c5eb44ea4924708415d1345396cc8291ec1f2
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-12 14:06:23 +0700

    :wrench: :fix mjs import

--- functions/router.ts
+++ functions/router.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from "fastify";
 import indexController from "./controller/indexController"
 import linehookController from "./controller/linehookController"
-import line from "@line/bot-sdk"
+import { Client } from "@line/bot-sdk"
 
 const channelToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
-const client = new line.Client({
+const client = new Client({
     channelAccessToken: channelToken!
 })
 
