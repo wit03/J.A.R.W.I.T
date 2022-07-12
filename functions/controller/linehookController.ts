801820de36c97e4c03a0c9873c42f123a212a9ea
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-12 13:23:23 +0700

    :sparkles: :migrate axios POST => linebotsdk

--- functions/controller/linehookController.ts
+++ functions/controller/linehookController.ts
@@ -1,29 +1,15 @@
 import { channel } from "diagnostic_channel";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import axios from "axios";
-import { request } from "http";
+import { client } from "../router";
 
-const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
+const channelToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
 
 export default async function linehookController(fastify: FastifyInstance) {
-  fastify.post("/", async (_request: any, reply: FastifyReply) => {
-    for(const event of _request.body.events) {
-        if(event.type === 'message') {
-            console.log(event)
-        }
-    }  
-    if (_request.body.type === "message") {
-      axios.post("https://api.line.me/v2/bot/message/reply", {
-        replyToken: _request.body.replyToken,
-        messages: [
-          { type: "text", text: "test" },
-        ],
-      }, {
-        headers: {
-          authorization: `Bearer ${channelAccessToken}`,
-        },
-      });
-    }
+   
+    client.replyMessage(channelToken!, {
+        type: "text",
+        text: "Test"
+    })
     return "ok";
-  });
+
 }
--- functions/router.ts
+++ functions/router.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from "fastify";
 import indexController from "./controller/indexController"
 import linehookController from "./controller/linehookController"
+import line from "@line/bot-sdk"
+
+const channelToken = process.env.LINE_CHANNEL_ACCESS_TOEKN;
+const client = new line.Client({
+    channelAccessToken: channelToken!
+})
 
 export default async function router(fastify: FastifyInstance) {
     fastify.register(indexController, { prefix: "/" });
     fastify.register(linehookController, { prefix: "/line-webhook" })
 }
+
+export { client }
