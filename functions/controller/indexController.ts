972886cf5bc8227c749ea482f138bd838de0327b
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-07-12 09:54:15 +0700

    :sparkles: :feat status code

--- functions/controller/indexController.ts
+++ functions/controller/indexController.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 
 export default async function indexController(fastify: FastifyInstance) {
     fastify.get("/",async (_request: FastifyRequest, reply: FastifyReply) => {
+        reply.status(200)
         reply.send("Hello World")
     })
 }
